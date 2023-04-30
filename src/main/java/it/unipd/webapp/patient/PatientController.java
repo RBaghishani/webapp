0155b6581204793dcab3c3ef2738e8ba88384aaf
Reihaneh <baghishani.r@gmail.com>
2023-04-30 13:20:22 +0430

    fix cross-origin

--- src/main/java/it/unipd/webapp/patient/PatientController.java
+++ src/main/java/it/unipd/webapp/patient/PatientController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping(path = "api/v1/patient")
 public class PatientController {
 
