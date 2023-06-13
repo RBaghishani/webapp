c988755de1f006e81d782bfa1d38f9e0d599e6f3
Reihaneh <baghishani.r@gmail.com>
2023-06-13 22:31:23 +0200

    add handleValidationExceptions

--- src/main/java/it/unipd/webapp/helpers/ErrorController.java
+++ src/main/java/it/unipd/webapp/helpers/ErrorController.java
@@ -1,5 +1,6 @@
 package it.unipd.webapp.helpers;
 
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -7,6 +8,8 @@ import it.unipd.webapp.exception.AuthenticationException;
 import it.unipd.webapp.exception.DuplicateEmailException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -47,4 +50,15 @@ public class ErrorController {
         return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(Collections.singletonMap("message", errors));
+    }
 }
